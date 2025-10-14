4d6b612e8a640343f79ec599b548c6b4bf76bee8
Seyeon CHO <crownjoe@naver.com>
2025-10-14 23:35:52 +0900

    [#2 Member 필드, 생성자, getter 추가

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -4,10 +4,16 @@ public class Member {
 
     private Long id;
     private String name;
+    private String email;
+    private String birthdate;
+    private Gender gender;
 
-    public Member(Long id, String name) {
+    public Member(Long id, String name, String email, String birthdate, Gender gender) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.gender = gender;
     }
 
     public Long getId() {
@@ -17,4 +23,17 @@ public class Member {
     public String getName() {
         return name;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
 }
