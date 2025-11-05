83b97f4dd214270d40b667884e6703c0a83f2015
Seyeon CHO <crownjoe@naver.com>
2025-11-05 17:08:31 +0900

    [#3] ArticleRepository 구현 및MemberRepository 수정

--- src/main/java/org/sopt/repository/MemberRepository.java
+++ src/main/java/org/sopt/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package org.sopt.repository;
 
 import org.sopt.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member save(Member member);
 
