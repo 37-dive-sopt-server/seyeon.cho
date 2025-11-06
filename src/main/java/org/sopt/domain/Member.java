5bdf5a5c646cbb764b6ba1e0023e8791222af11b
Seyeon CHO <crownjoe@naver.com>
2025-11-06 23:24:50 +0900

    [#3] lombok 고려한 Article, Member 도메인 수정

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -1,75 +1,44 @@
 package org.sopt.domain;
 
 import jakarta.persistence.*;
-
+import lombok.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
-    private LocalDate birthdate;
-
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Gender gender;
 
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
     @OneToMany(mappedBy = "member")
+    @Builder.Default
     private List<Article> articles = new ArrayList<>();
 
-
-    public  Member() {
-
-    }
-
-    public Member(Long id, String name, String email, LocalDate birthdate, Gender gender) {
-
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("이름은 필수입니다.");
-        }
-        if (email == null || !email.contains("@")) {
-            throw new IllegalArgumentException("올바른 이메일이 아닙니다.");
-        }
-        if (birthdate == null) {
-            throw new IllegalArgumentException("생년월일은 필수입니다.");
-        }
-
-        this.id = id;
+    public Member(String name, String email, LocalDate birthDate, Gender gender) {
         this.name = name;
         this.email = email;
-        this.birthdate = birthdate;
+        this.birthDate = birthDate;
         this.gender = gender;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public boolean hasEmail(String email) {
-        return this.email.equals(email);
+        this.articles = new ArrayList<>();
     }
 }
